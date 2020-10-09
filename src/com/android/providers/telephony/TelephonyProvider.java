d962d76faea3cd3302fe1bbe3fb8df51faa5c745
Michael Bestas <mkbestas@lineageos.org>
2020-10-09 19:51:58 +0300

    TelephonyProvider: add upgrade support from lineage-17.1
    * Only support upgrades from the latest 17.1 db schema
    
    Change-Id: I202917fb4e355afc9a0c53b32d252743df38da16

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -1651,7 +1651,9 @@ public class TelephonyProvider extends ContentProvider
                 oldVersion = 41 << 16 | 6;
             }
 
-            if (oldVersion < (42 << 16 | 6)) {
+            if (oldVersion < (44 << 16 | 6)) {
+                // In Lineage 17.1, we changed the version to 43
+                // so the AOSP version 42 and 43 upgrade would be skipped
                 try {
                     // Try to update the siminfo table. It might not be there.
                     db.execSQL("ALTER TABLE " + SIMINFO_TABLE + " ADD COLUMN " +
@@ -1662,9 +1664,7 @@ public class TelephonyProvider extends ContentProvider
                                 "The table will get created in onOpen.");
                     }
                 }
-            }
 
-            if (oldVersion < (43 << 16 | 6)) {
                 try {
                     // Try to update the siminfo table. It might not be there.
                     db.execSQL("ALTER TABLE " + SIMINFO_TABLE + " ADD COLUMN "
@@ -1676,10 +1676,7 @@ public class TelephonyProvider extends ContentProvider
                                 "The table will get created in onOpen.");
                     }
                 }
-                oldVersion = 43 << 16 | 6;
-            }
 
-            if (oldVersion < (44 << 16 | 6)) {
                 try {
                     // Try to update the siminfo table. It might not be there.
                     db.execSQL("ALTER TABLE " + SIMINFO_TABLE + " ADD COLUMN "
